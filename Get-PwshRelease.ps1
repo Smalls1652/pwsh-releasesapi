f68dd88b4ea03046b3174cb5b2a560fd6c61cab0
smalls1652 <smalls.online@icloud.com>
2020-01-21 20:39:59 -0500

    Added comments and fixed a typo.
    - Added simple comments throughout the code.
    - Fixed a typo where 'Determining' was spelled incorrectly.

--- Get-PwshRelease.ps1
+++ Get-PwshRelease.ps1
@@ -20,8 +20,10 @@ begin {
         [string]$ReleaseDownloadUri
     }
 
+    #RegEx pattern for parsing the Releases API header for page numbers.
     $ReleaseApiPagesRegex = "<(?'apiPageUri'.+\?page=(?'pageNumber'\d{1,}))>; rel=\`"(?'rel'.+)\`""
 
+    #RegEx patterns for Windows, Linux, and macOS release asset parsing.
     $WindowsReleaseRegex = ".+-(?'platform'win)-(?'arch'.+)(?'extensions'(\.tar\.gz|\..{3,4}))"
     $LinuxReleaseRegex = ".+-(?'platform'linux)-(?'arch'(alpine-.+|.+))(?'extensions'(\.tar\.gz|\..{3,4}))"
     $macOSReleaseRegex = ".+-(?'platform'osx)-(?'arch'.+)(?'extensions'(\.tar\.gz|\..{3,4}))"
@@ -30,10 +32,12 @@ begin {
 process {
     $ReleasesApiUri = "https://api.github.com/repos/powershell/powershell/releases"
 
+    #Making an initial call to the releases API to get the total pages to process.
     Write-Verbose "Starting API call to GitHub."
     $ReleasesApiStart = Invoke-WebRequest -UseBasicParsing -Uri $ReleasesApiUri -Verbose:$false
 
-    Write-Verbose "Determing the number of pages to process."
+    #Parsing the 'Link' property in the response header.
+    Write-Verbose "Determining the number of pages to process."
     $LastPageNumber = $null
     foreach ($relLink in ($ReleasesApiStart.Headers.Link -split ", ")) {
         $ApiPagesMatch = [regex]::Match($relLink, $ReleaseApiPagesRegex)
@@ -41,6 +45,7 @@ process {
         $relName = $ApiPagesMatch.Groups | Where-Object -Property "Name" -eq "rel"
         $pageNum = $ApiPagesMatch.Groups | Where-Object -Property "Name" -eq "pageNumber"
 
+        #If 'rel' = 'last', set the last page number from the URI in the property.
         switch ($relName.Value) {
             "last" {
                 $LastPageNumber = $pageNum.Value
@@ -55,8 +60,8 @@ process {
     }
     Write-Verbose "Total number of pages: $($LastPageNumber)."
 
+    #Gather all of the results from each page.
     $i = 1
-
     $ApiResults = @()
     while ($i -le $LastPageNumber) {
         Write-Verbose "Getting results from page $($i)/$($LastPageNumber)."
@@ -66,11 +71,13 @@ process {
         $i++
     }
 
+    #Parse each release returned from the API.
     $returnObj = @()
-
     foreach ($ApiResult in $ApiResults) {
         Write-Verbose "Processing Result - '$($ApiResult.name)'"
         $ReleaseAssetObjs = @()
+
+        #Parsing for objects with names like '*-win-*' for Windows releases.
         foreach ($obj in ($ApiResult.assets | Where-Object -Property "name" -Like "*-win-*")) {
             $releaseRegex = [regex]::Match($obj.name, $WindowsReleaseRegex)
 
@@ -87,6 +94,7 @@ process {
             $ReleaseAssetObjs += $WinReleaseObj
         }
 
+        #Parsing for objects with names like '*-linux-*' for generic Linux releases.
         foreach ($obj in ($ApiResult.assets | Where-Object -Property "name" -Like "*-linux-*")) {
             $releaseRegex = [regex]::Match($obj.name, $LinuxReleaseRegex)
 
@@ -103,6 +111,7 @@ process {
             $ReleaseAssetObjs += $LinuxReleaseObj
         }
 
+        #Parsing for objects with names like '*-osx-*' for macOS releases.
         foreach ($obj in ($ApiResult.assets | Where-Object -Property "name" -Like "*-osx-*")) {
             $releaseRegex = [regex]::Match($obj.name, $macOSReleaseRegex)
 
@@ -119,13 +128,13 @@ process {
             $ReleaseAssetObjs += $macOSReleaseObj
         }
 
+        #Build the release object
         $ReleaseObj = [Release]::new()
         $ReleaseObj.Name = $ApiResult.name
         $ReleaseObj.VersionTag = $ApiResult.tag_name
         $ReleaseObj.ReleaseDate = [datetime]::Parse($ApiResult.published_at)
         $ReleaseObj.IsPreview = $ApiResult.prerelease
         $ReleaseObj.ReleasePageUri = $ApiResult.html_url
-
         $ReleaseObj.ReleaseAssets = $ReleaseAssetObjs
 
         $returnObj += $ReleaseObj
